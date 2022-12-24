41902f39ccbb450bedd6377f6c30968f6a977458
victorluk72 <victorluk72@yahoo.ca>
2022-12-24 14:54:44 -0500

    First live version

--- tools.go
+++ tools.go
@@ -1,7 +1,9 @@
 package toolkit
 
 import (
+	"bytes"
 	"crypto/rand"
+	"encoding/json"
 	"errors"
 	"fmt"
 	"io"
@@ -20,11 +22,13 @@ import (
 const randomStringSource = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789_+"
 
 // Tools is the custom type used to instantiate this module.
-// Any variables of this type will have access to all methods with reciever *Tools
+// Any variables of this type will have access to all methods with reciever *Tools [func(t *Tools){}]
 // This technic used to share methods from the modules with other programs
 type Tools struct {
-	MaxFileSize      int
-	AllowedFileTypes []string //You can provide what type of file you allow (imgs/pdf/docs etc)
+	MaxFileSize        int
+	AllowedFileTypes   []string //You can provide what type of file you allow (imgs/pdf/docs etc)
+	MaxJSONSize        int      // Provide maximum size of JSON payload
+	AllowUnknownFields bool     // allow or not unknown fields in JSON payload
 }
 
 // RandomStringGenerator generates random string of certain length
@@ -317,8 +321,207 @@ func (t *Tools) DownloadStaticFile(w http.ResponseWriter, r *http.Request, p, fi
 
 //-----------END OF DOWNLOAD FILE SECTION------------------------------
 
-//*******************************************************************
-//-----------BEGINING OF IMAGE PLOAD SECTION-------------------------
-// This function used to upload files from browser to server
+//-----------BEGINING OF JSON READ SECTION-------------------------
+// This function reads the data from JSON and convert it to Go structure
 
-//-----------END OF FILE UPLOAD SECTION------------------------------
+// Stuct for response we send back to server. Two first fileds are mandatory, Data is optional
+type JSONResponse struct {
+	Error   bool        `json:"error"`
+	Message string      `json:"message"`
+	Data    interface{} `json:"data,omitempty"`
+}
+
+// ReadJSON allows to read incoming JSON payloads (as a body of incoming request)
+// and converts to provided Go data structure
+func (t *Tools) ReadJSON(w http.ResponseWriter, r *http.Request, data interface{}) error {
+
+	// 1.0 Limit max size of the incoming JSON
+	maxBytesJson := 1024 * 1024 // around 1 MGB of payload size
+
+	if t.MaxJSONSize != 0 {
+		maxBytesJson = t.MaxJSONSize
+	}
+
+	// 1.1 Read request body and check if it less than size we identified
+	// MaxBytesReader prevents clients from accidentally or maliciously sending a large request
+	r.Body = http.MaxBytesReader(w, r.Body, int64(maxBytesJson))
+
+	// 1.2 decode payload from request Body
+	dec := json.NewDecoder(r.Body)
+
+	// this part check if paramer "AllowUnknownFields" from our Tools
+	// is false, then it forces our decoded result reject unknown fields
+	// we won't process JSON fields that we don't know about
+	if !t.AllowUnknownFields {
+		dec.DisallowUnknownFields()
+	}
+
+	// 1.3 Take our decoded values and turn them to "data" (the struct we provided as a function parameter)
+	// ^Decode reads the next JSON-encoded value from its input and stores it in the value
+	// !->This is main comand that turns decoded JSON to Golang data struct
+	err := dec.Decode(data)
+	if err != nil {
+		// improve error handling
+		// ^A SyntaxError is a description of a JSON syntax error
+		// ^An UnmarshalTypeError describes a JSON value that was not appropriate for a value of a specific Go type
+		var syntaxError *json.SyntaxError
+		var unmarshalTypeError *json.UnmarshalTypeError
+		var invalidUnmarshalError *json.InvalidUnmarshalError
+
+		switch {
+		case errors.As(err, &syntaxError):
+			return fmt.Errorf("body contains badly-formed JSON (at character %d)", syntaxError.Offset)
+
+		case errors.Is(err, io.ErrUnexpectedEOF):
+			return errors.New("body contains badly formed JSON")
+
+		case errors.As(err, &unmarshalTypeError):
+			if unmarshalTypeError.Field != "" {
+				return fmt.Errorf("body contains incorrect JSON type for field %q", unmarshalTypeError.Field)
+			}
+			return fmt.Errorf("body contains incorrect JSON type for field %d", unmarshalTypeError.Offset)
+
+		// check if there is no body at all
+		case errors.Is(err, io.EOF):
+			return errors.New("JSON body must not be empty")
+
+		// check for unknown field in the body
+		case strings.HasPrefix(err.Error(), "json: unknown field"):
+			fieldName := strings.TrimPrefix(err.Error(), "json: unknown field")
+			return fmt.Errorf("body contains unknown key %s", fieldName)
+
+		// check if request body is too large
+		case err.Error() == "http: request body too large":
+			return fmt.Errorf("body must not be larger then %d bytes", maxBytesJson)
+
+		case errors.As(err, &invalidUnmarshalError):
+			return fmt.Errorf("error unmarshaling JSON: %s", err.Error())
+
+		default:
+			return err
+		}
+
+	}
+
+	// 1.4 Check if we only recieving one file
+	err = dec.Decode(&struct{}{})
+	if err != io.EOF {
+		return errors.New("body should contain only one JSON value")
+	}
+
+	return nil
+
+}
+
+//-----------END OF JSON READ SECTION------------------------------
+
+//-----------BEGINING OF JSON WRITE SECTION-------------------------
+// This function used to write JSON back to response
+
+// WriteJSON takes some data (as function parameter, and writes it to http response along with status code)
+// header is variadic parameter for http headers. We might send headers or might not
+func (t *Tools) WriteJSON(w http.ResponseWriter, status int, data interface{}, headers ...http.Header) error {
+
+	// we take our data (some struct and turm it (marshal) to JSON)
+	out, err := json.Marshal(data)
+	if err != nil {
+		return err
+	}
+
+	// we need to check if have any "headers" paramenter in our function parameters and send it as a header
+	// back in our resonse
+	if len(headers) > 0 {
+		for key, value := range headers[0] {
+			w.Header()[key] = value //key is what the header type is, and value is what it set to
+		}
+	}
+
+	// now set content type of our response to "applicatiom/json"
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(status)
+
+	// !->This is main comand that writes our data to http respnse as a JSON
+	_, err = w.Write(out)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+//-----------END OF JSON WRITE SECTION------------------------------
+
+//-----------BEGINING OF JSON ERROR SECTION-------------------------
+// This function write the error to JSON resonse
+
+// ErrorJSON takes an error and optionally status code, then generates JSON resonse
+// and sends as http response
+func (t *Tools) ErrorJSON(w http.ResponseWriter, err error, status ...int) error {
+
+	//this is our defualt status code
+	statusCode := http.StatusBadRequest // status 400
+
+	// check if stasu code exosts as function parameter send it as a status code
+	if len(status) > 0 {
+		statusCode = status[0]
+	}
+
+	//variable payload of type JSONResponse (our custom type we created at line 327)
+	var payload JSONResponse
+
+	// build the payload that we send back
+	payload.Error = true
+	payload.Message = err.Error() //the message of payload is our error
+
+	return t.WriteJSON(w, statusCode, payload)
+
+}
+
+//-----------END OF JSON ERROR SECTION------------------------------
+
+// -----------BEGINING OF JSON ERROR SECTION-------------------------
+// This function write the error to JSON resonse
+
+// PushJSONToRemote takes URL, some Data and optional client and pushes data to remote sever
+// If no client is specified we use standard http.Client
+// Function returns the resonse (as *http.Response type) and status code (e.g 200)
+func (t *Tools) PushJSONToRemote(uri string, data interface{}, client ...*http.Client) (*http.Response, int, error) {
+
+	// create JSON from our "data" function parameter (must be struct)
+	jsonData, err := json.Marshal(data)
+	if err != nil {
+		return nil, 0, errors.New("error building JSON")
+
+	}
+
+	// check for custom http client
+	httpClient := &http.Client{}
+
+	// check if client is part of function parameter
+	// if it's exists asign our variable to it
+	if len(client) > 0 {
+		httpClient = client[0]
+	}
+
+	// build the request (with headers)
+	request, err := http.NewRequest("POST", uri, bytes.NewBuffer(jsonData))
+	if err != nil {
+		return nil, 0, errors.New("error sending JSON to uri")
+
+	}
+	// set headers
+	request.Header.Set("Content-Type", "application/json")
+
+	// sent it to remote server (as POST), it returns response back
+	// ^Do sends an HTTP request and returns an HTTP response, following
+	// policy (such as redirects, cookies, auth) as configured on the client.
+	response, err := httpClient.Do(request)
+	if err != nil {
+		return nil, 0, errors.New("error gettng response from remote server")
+
+	}
+	defer response.Body.Close()
+
+	// retrieve/return the resoponse
+	return response, response.StatusCode, nil
+
+}
